68d38775bab5f37b93eb59c9deb67cf8a07185ab
Aleksandr Petrovskiy <uiautomation@hotmail.com>
2024-06-16 01:31:08 +0300

    plan for implementation

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -8,5 +8,22 @@ var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(_ string) (string, error) {
 	// Place your code here.
+
+	// TODO: wrong cases
+	/*
+		1. first digit
+		2. doubled digits, multiple digits
+		3. no letters
+		4. back ticks without digits or escapes
+	*/
+	// TODO: use cases
+	/*
+		1. letters only
+		2. letters and digits
+		3. special characters like \n
+		4. empty string
+		5. zero (removal of a letter)
+		6. back ticks and escaped digits or escapes
+	*/
 	return "", nil
 }
